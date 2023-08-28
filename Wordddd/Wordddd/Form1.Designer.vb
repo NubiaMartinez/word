67c0d732722e6a6a398bb18d5233a4dfdc38d0f9
NubiaMartinez <118578334+NubiaMartinez@users.noreply.github.com>
2023-08-28 03:05:45 -0600

    Add files via upload

---
+++ Wordddd/Wordddd/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(188, 22)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(123, 21)
+        Me.label1.TabIndex = 3
+        Me.label1.Text = "Ingresar texto:"
+        '
+        'textBox1
+        '
+        Me.textBox1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textBox1.Location = New System.Drawing.Point(125, 60)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(262, 27)
+        Me.textBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(155, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 31)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Word"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(272, 102)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 31)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Excel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(557, 155)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.textBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents label1 As Label
+    Private WithEvents textBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Wordddd/Wordddd/Form1.vb
@@ -0,0 +1,39 @@
+﻿
+Public Class Form1
+    Dim dialogo As New SaveFileDialog()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Word = New Microsoft.Office.Interop.Word.Application
+        Dim wordApp = Word.Application()
+
+
+        Dim ruta, dato As String
+        If (dialogo.ShowDialog() <> DialogResult.OK) Then
+            Return
+        End If
+        ruta = dialogo.FileName
+        wordApp.Visible = True
+        wordApp.Documents.Add()
+        dato = textBox1.Text
+
+        wordApp.Selection.TypeText(dato)
+        wordApp.ActiveDocument.SaveAs2(ruta)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Excel = New Microsoft.Office.Interop.Excel.Application
+        Dim excelApp = Excel.Application()
+        Dim ruta As String
+        If (dialogo.ShowDialog() <> DialogResult.OK) Then
+            Return
+        End If
+        ruta = dialogo.FileName
+        excelApp.Visible = True
+
+        Dim workbook = excelApp.Workbooks.Add()
+        Dim worksheet = workbook.ActiveSheet
+
+        worksheet.Cells(1, 1) = "Caja de texto"
+        worksheet.Cells(2, 1) = textBox1.Text
+    End Sub
+End Class
